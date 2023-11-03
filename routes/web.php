25e0bcfa1184359202cf1e8598012b29ca8add2c
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-11-03 09:25:15 +0800

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create', [NoteController::class, 'show']);
+Route::post('/create', [NoteController::class, 'store']);
+
+// Страница для получения всех заметок (GET-запрос)
+Route::get('/table', [NoteController::class, 'showData']);
